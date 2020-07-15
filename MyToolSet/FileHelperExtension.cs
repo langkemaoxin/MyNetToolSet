5a2dc4b436ed7bce200b171b540d0ed912b04bee
jzfz\chengongyi <chengongyi@jzfz.com.cn>
2020-07-15 16:43:03 +0800

    [Feature]添加文件新增

--- MyToolSet/FileHelperExtension.cs
+++ MyToolSet/FileHelperExtension.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.IO;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using System.Text;
 
 namespace MyToolSet
 {
@@ -46,6 +47,76 @@ namespace MyToolSet
             return GetFileBelongs(new FileInfo(fileInfo));
         }
 
-     
+        public static void CreateFile(string filePath)
+        {
+            AppendToFile(filePath, "");
+        }
+
+        /// <summary>
+        ///     往文件添加一行内容
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="content"></param>
+        public static void AppendToFile(string filePath, string content)
+        {
+            if (string.IsNullOrWhiteSpace(content)) return;
+
+            if (!File.Exists(filePath))
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite,
+                    4096, true))
+                {
+                    using (var streamWriter = new StreamWriter(fileStream, Encoding.GetEncoding("gb2312")))
+                    {
+                        streamWriter.WriteLine(content);
+                    }
+                }
+
+                return;
+            }
+
+            try
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite,
+                    4096, true))
+                {
+                    using (var streamWriter = new StreamWriter(fileStream, Encoding.GetEncoding("gb2312")))
+                    {
+                        streamWriter.WriteLine(content);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+        }
+
+        /// <summary>
+        ///     读取文件的第一行
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static string ReadSingleLineFromFile(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new Exception($"文件路径不存在:{filePath}");
+
+            try
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite,
+                    4096, true))
+                {
+                    using (var stringReader = new StreamReader(fileStream, Encoding.GetEncoding("gb2312")))
+                    {
+                        var readLine = stringReader.ReadLine();
+                        return readLine;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
     }
 }
